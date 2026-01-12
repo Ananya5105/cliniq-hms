30bc5b6ff8d16405c8c30fb2abad677a84d4c278
Ananya5105 <195567760+Ananya5105@users.noreply.github.com>
2026-01-12 05:38:05 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ auth.js
@@ -0,0 +1,10 @@
+<script>
+  if (localStorage.getItem("loggedIn") !== "true") {
+    window.location.href = "login.html";
+  }
+
+  function logout() {
+    localStorage.removeItem("loggedIn");
+    window.location.href = "login.html";
+  }
+</script>
